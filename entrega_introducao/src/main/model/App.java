dca51a20f982662d6f0d2dc981d054801f23b228
Stephanie345 <stephanie.costa.moura@hotmail.com>
2020-06-19 22:20:44 -0300

    Add files via upload
    adicionado o arquivo app.java

--- entrega_introducao/src/main/model/App.java
+++ entrega_introducao/src/main/model/App.java
@@ -1,6 +1,20 @@
 package model;
-public class App{
-	public static void main(String [] args){
-		System.out.println("Ola mundo");
-	}	
+
+
+import org.apache.commons.text.WordUtils;
+
+
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println(WordUtils.capitalize("hello world"));
+		System.out.println(WordUtils.capitalize("stephanie"));
+
+	}
+	public String capitalLetter(String text){
+		String letraMaiuscula = text;
+		String resultado = WordUtils.capitalize(letraMaiuscula);
+		return resultado;
+	}
+	
 }
