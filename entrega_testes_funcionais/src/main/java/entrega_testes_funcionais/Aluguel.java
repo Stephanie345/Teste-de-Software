0c4a90bdbd4f3496d97e91830d5562be95daa1b7
stephanie <stephanie.costa.moura@hotmail.com>
2020-07-12 14:03:13 -0300

    Adicionado a classe Aluguel

---
+++ entrega_testes_funcionais/src/main/java/entrega_testes_funcionais/Aluguel.java
@@ -0,0 +1,22 @@
+package entrega_testes_funcionais;
+
+
+public class Aluguel{
+	public float valorNominal;
+	public int dia;
+
+	public float getValor_nominal(){
+		return valorNominal;
+	}
+	public void setValor_nominal(float valorNominal){
+		this.valorNominal=valorNominal;
+	}
+	public int getDia(){
+		return dia;
+	}
+	public void setDia(int dia){
+		this.dia=dia;
+	}
+
+}
+
